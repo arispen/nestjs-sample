102c01a572d50298e6fb748a82de63958aaa4a29
arispen <arispen@gmail.com>
2021-10-13 17:31:00 +0200

    cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    CatsModule,
+  ],
 })
 export class AppModule {}
